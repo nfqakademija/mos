4eaaa11767f814bfdf144254b8210fd6e357ed84
Vaidas A <vaidastr@gmail.com>
2018-10-30 12:32:13 +0200

    Anonymous restricted only to Login page. Added profile view

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProfileController extends AbstractController
+{
+    /**
+     * @Route("/profile/view/{user}",
+     *   name="profile.view",
+     *   methods="GET",
+     *   requirements={"user" = "\d+"})
+     * @var User $user
+     * @return
+     */
+    public function profileView(User $user)
+    {
+
+        return $this->render('profile/view.html.twig', [
+            'user' => $user,
+        ]);
+    }
+}
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -22,4 +22,11 @@ class SecurityController extends AbstractController
           'last_username' => $lastUsername
         ]);
     }
+
+  /**
+   * @Route("/logout", name="logout")
+   */
+    public function logout() {
+
+    }
 }
