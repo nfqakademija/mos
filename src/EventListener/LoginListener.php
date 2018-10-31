5d6ec26b6f720870faf2afa698773f0a966b44e0
Vaidas A <vaidastr@gmail.com>
2018-10-31 09:24:19 +0200

    Changed to Listener. Doesn't work login anyway.

--- src/EventListener/LoginListener.php
+++ src/EventListener/LoginListener.php
@@ -4,7 +4,7 @@ namespace App\EventListener;
 
 
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+use Symfony\Component\Security\Core\Event\AuthenticationEvent;
 use App\Entity\User;
 
 class LoginListener
@@ -17,18 +17,21 @@ class LoginListener
     $this->em = $em;
   }
 
-  public function onSecurityInteractiveLogin(InteractiveLoginEvent $event)
+  public function onSecurityAuthenticationSuccess(AuthenticationEvent $event)
   {
 
     // Get the User entity.
 
     /** @var User $user */
     $user = $event->getAuthenticationToken()->getUser();
-    // Update your field here.
-    $user->setLastAccessDate(new \DateTime());
+    if($user instanceof User ) { //on logout we don't get a User object
+      // Update your field here.
+      $user->setLastAccessDate(new \DateTime());
 
-    // Persist the data to database.
-    $this->em->persist($user);
-    $this->em->flush();
+      // Persist the data to database.
+      $this->em->persist($user);
+      $this->em->flush();
+  }
+
   }
 }
