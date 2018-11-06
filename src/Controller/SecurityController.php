3fbc2a8545a38ee4338e9dc5bfd8fce19b00834e
Vaidas A <vaidastr@gmail.com>
2018-11-06 19:44:00 +0200

    Auth seems working

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,25 +10,25 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    /**
-     * @Route("/login", name="login")
-     */
-    public function login(Request $request, AuthenticationUtils $utils)
-    {
-      $error = $utils->getLastAuthenticationError();
-      $lastUsername = $utils->getLastUsername();
-        return $this->render('security/login.html.twig', [
-          'error' => $error,
-          'last_username' => $lastUsername
-        ]);
-    }
-
-  /**
-   * @Route("/logout", name="logout")
-   */
-    public function logout() {
-
-    }
+//    /**
+//     * @Route("/login", name="login")
+//     */
+//    public function login(Request $request, AuthenticationUtils $utils)
+//    {
+//      $error = $utils->getLastAuthenticationError();
+//      $lastUsername = $utils->getLastUsername();
+//        return $this->render('security/login.html.twig', [
+//          'error' => $error,
+//          'last_username' => $lastUsername
+//        ]);
+//    }
+//
+//  /**
+//   * @Route("/logout", name="logout")
+//   */
+//    public function logout() {
+//
+//    }
 
 
 }
