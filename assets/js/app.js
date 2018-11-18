6c5c981678cad8fc62c9b506d94611e5e9a26a38
Mantas Arlauskas <mantasarlauskas1997@gmail.com>
2018-11-18 13:04:39 +0200

    Fixed group form issues

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,78 +3,51 @@ const $ = require('jquery');
 const randomString = require('random-string');
 
 $(document).ready(function() {
-
-
-
   $('.participants').on('click', '.participant__toggle-additional-button', function(e) {
     e.preventDefault();
-    $('.participant__additional').toggle();
+    $(this).parent().find('.participant__additional').toggle();
   });
 
   $('.participants').on('click', '.participant__username-generate-button', function(e) {
     e.preventDefault();
-    generateUsername();
+    $(this).parent().find('.participant__username').val(randomString());
   });
 
   $('.participants').on('click', '.participant__password-generate-button', function(e) {
     e.preventDefault();
-    generatePasword();
+    console.log($(this).html());
+    $(this).parent().find('.participant__password').val(randomString());
   });
 
-  // Get the ul that holds the collection of tags
   const $collectionHolder = $('div.participants');
   const $addParticipantLink = $('.participant__add-button');
 
-  // count the current form inputs we have (e.g. 2), use that as the new
-  // index when inserting a new item (e.g. 2)
   $collectionHolder.data('index', $collectionHolder.find(':input').length);
 
-  $addParticipantLink.on('click', function(e) {
-    // prevent the link from creating a "#" on the URL
+  $('.participants').on('click', '.participant__add-button', function(e) {
     e.preventDefault();
-
-    // add a new tag form (see code block below)
     addTagForm($collectionHolder, $addParticipantLink);
-
-    generateUsername();
-    generatePasword();
+    $(this).prev().find('.participant__username').val(randomString());
+    $(this).prev().find('.participant__password').val(randomString());
   });
 
+  $('.participants').on('click', '.participant__remove-button', function(e) {
+    e.preventDefault();
+    $(this).parent().remove();
+  });
 });
 
 function addTagForm($collectionHolder, $addParticipantLink) {
-  // Get the data-prototype explained earlier
   const prototype = $collectionHolder.data('prototype');
 
-  // get the new index
   const index = $collectionHolder.data('index');
 
-  // Replace '$$name$$' in the prototype's HTML to
-  // instead be a number based on how many items we have
   const newForm = prototype.replace(/__name__/g, index);
 
-  // increase the index with one for the next item
   $collectionHolder.data('index', index + 1);
 
-  // Display the form in the page in an li, before the "Add a tag" link li
   const $newFormLi = $('<div class="participant"></div>').append(newForm);
 
   $addParticipantLink.before($newFormLi);
-
-  // handle the removal, just for this example
-  $('.participants').on('click', '.participant__remove-button',function(e) {
-    console.log("wtf");
-    e.preventDefault();
-
-    $(this).parent().remove();
-  });
-}
-
-function generateUsername() {
-  $('.participant__username').val(randomString());
-}
-
-function generatePasword() {
-  $('.participant__password').val(randomString());
 }
 
