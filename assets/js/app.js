bdd1bea9cf82b7837b2197bb4fdb828ceeab8621
Mantas Arlauskas <mantasarlauskas1997@gmail.com>
2018-11-21 23:33:59 +0200

    Changed group form appearance

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,46 +1,15 @@
-require('bootstrap');
 require('materialize-css/dist/js/materialize.min');
 const $ = require('jquery');
 const randomString = require('random-string');
 
 $(document).ready(function() {
-
-  $(".container .alert-success").fadeTo(2000, 500).slideUp(500, function(){
-    $(".container .alert-success").slideUp(500);
-  });
-
-  $('.participants').on('click', '.participant__toggle-additional-button', function(e) {
-    e.preventDefault();
-    $(this).parent().parent().find('.participant__additional').toggle();
-    $(this).text() === 'More' ? $(this).text('Less') : $(this).text('More');
-  });
-
-  $('.participants').on('click', '.participant__username-generate-button', function(e) {
-    e.preventDefault();
-    $(this).parent().parent().find('.participant__username').val(randomString());
-  });
-
-  $('.participants').on('click', '.participant__password-generate-button', function(e) {
-    e.preventDefault();
-    $(this).parent().parent().find('.participant__password').val(randomString());
-  });
+  $('.participant__additional').hide();
 
   const $collectionHolder = $('div.participants');
-  const $addParticipantLink = $('.participant__add-button');
+
+  clickEvents($collectionHolder);
 
   $collectionHolder.data('index', $collectionHolder.find(':input').length);
-
-  $('.participants').on('click', '.participant__add-button', function(e) {
-    e.preventDefault();
-    addTagForm($collectionHolder, $addParticipantLink);
-    $(this).prev().find('.participant__username').val(randomString());
-    $(this).prev().find('.participant__password').val(randomString());
-  });
-
-  $('.participants').on('click', '.participant__remove-button', function(e) {
-    e.preventDefault();
-    $(this).parent().parent().remove();
-  });
 });
 
 function addTagForm($collectionHolder, $addParticipantLink) {
@@ -55,5 +24,58 @@ function addTagForm($collectionHolder, $addParticipantLink) {
   const $newFormLi = $('<div class="participant"></div>').append(newForm);
 
   $addParticipantLink.before($newFormLi);
+}
+
+function clickEvents($collectionHolder) {
+  $(".container .alert-success").fadeTo(2000, 500).slideUp(500, function(){
+    $(".container .alert-success").slideUp(500);
+  });
+
+  $('.participants').on('click', '.participant__toggle-additional-button', function(e) {
+    e.preventDefault();
+    $(this).parent().parent().parent().find('.participant__additional').toggle();
+
+    if($(this).text() === 'Less') {
+      $(this).text('More');
+      $(this).parent().parent().parent().removeClass('participant--active z-depth-5');
+    } else {
+      $(this).text('Less');
+      $(this).parent().parent().parent().addClass('participant--active z-depth-5');
+    }
+  });
+
+  $('.participants').on('click', '.participant__username-generate-button', function(e) {
+    e.preventDefault();
+    $(this).parent().parent().find('.participant__username').val(randomString());
+  });
+
+  $('.participants').on('click', '.participant__password-generate-button', function(e) {
+    e.preventDefault();
+    $(this).parent().parent().find('.participant__password').val(randomString());
+  });
+
+  $('.participants').on('click', '.participants__add-button', function(e) {
+    e.preventDefault();
+    addTagForm($collectionHolder, $('.participants__add-button'));
+    $(this).prev().find('.participant__username').val(randomString());
+    $(this).prev().find('.participant__password').val(randomString());
+    M.updateTextFields();
+  });
+
+  $('.participants').on('click', '.participant__remove-button', function(e) {
+    e.preventDefault();
+    $(this).parent().parent().parent().remove();
+  });
+
+  $('.participants').on('keydown', '.participant__surname', function(e) {
+    const keyCode = e.keyCode || e.which;
+
+    if ((keyCode === 9 || keyCode === 13) && !$(this).parent().parent().parent().next().is('.participant')) {
+      addTagForm($collectionHolder, $('.participants__add-button'));
+
+      $(this).prev().find('.participant__username').val(randomString());
+      $(this).prev().find('.participant__password').val(randomString());
+    }
+  });
 }
 
