abfd3413bebad9e2140efcf45064a77d5fc122bb
Mantas Arlauskas <mantasarlauskas1997@gmail.com>
2018-12-13 11:58:52 +0200

    Style changes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,6 +12,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return $this->render('base.html.twig');
+        return $this->redirectToRoute('group.viewlist');
     }
 }
