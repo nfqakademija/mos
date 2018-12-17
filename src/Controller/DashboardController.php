aa99c42cca4984a4841d54d628ba6ffabf2a747c
Vaidas A <vaidastr@gmail.com>
2018-12-17 21:57:27 +0200

    Started dashboard

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class DashboardController
+ *
+ * @package App\Controller
+ */
+class DashboardController extends AbstractController
+{
+    /**
+     * @Route("/dashboard", name="dashboard",)
+     */
+    public function dashboard(
+
+    ) {
+        
+        return $this->render('other/dashboard.html.twig', [
+        ]);
+    }
+
+
+    /**
+     * @Route("/report/participants/export", name="report.participants.export",)
+     * @param Request $request
+     * @param ParticipantsReportManager $report
+     * @param Helper $helper
+     * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
+     * @throws \PhpOffice\PhpSpreadsheet\Exception
+     * @throws \PhpOffice\PhpSpreadsheet\Writer\Exception
+     */
+    public function participantsReportToExcel(Request $request, ParticipantsReportManager $report, Helper $helper)
+    {
+        $datesFromTo = $helper->dataFromRequest($request);
+
+        $result = $report->reportToExcel($datesFromTo['dateFrom'], $datesFromTo['dateTo']);
+
+        // Return the excel file as an attachment
+        return $this->file(
+            $result['file'],
+            $result['file_name'],
+            ResponseHeaderBag::DISPOSITION_INLINE
+        );
+    }
+
+    /**
+     * @Route("/report/status", name="report.status")
+     * @param StatusReportManager $report
+     * @param RegionRepository $regionRepository
+     * @return Response
+     */
+    public function statusReport(StatusReportManager $report, RegionRepository $regionRepository)
+    {
+        $status = $report->getStatusReport($regionRepository);
+
+        return $this->render('report/status.html.twig', [
+            'status' => $status,
+        ]);
+    }
+
+    /**
+     * @Route("/report/schedule", name="report.schedule")
+     * @param Request $request
+     * @param PaginatorInterface $paginator
+     * @param TimeSlotRepository $ts
+     * @param Helper $helper
+     * @return Response
+     * @throws \Exception
+     */
+    public function scheduleReport(
+        Request $request,
+        PaginatorInterface $paginator,
+        TimeSlotRepository $ts,
+        RegionRepository $regionRepository,
+        Helper $helper
+    ) {
+        $regions = $regionRepository->getAllRegionsUsedInGroups();
+        $dataFromRequest = $helper->dataFromRequest($request);
+        $submitButton = $request->query->get('submit_button');
+        if ($submitButton === 'export') {
+            $response = $this->forward('App\Controller\ReportController::scheduleReportToExcel', [
+                'dateFrom' => $dataFromRequest['dateFrom'],
+                'dateTo' => $dataFromRequest['dateTo'],
+                'regionIds' => $dataFromRequest['regionIds'],
+            ]);
+        } else {
+            $page = $helper->getPageFromRequest($request);
+
+            $query = $ts->getTimeSlotsInPeriod($dataFromRequest['dateFrom'], $dataFromRequest['dateTo'], $dataFromRequest['regionIds']);
+            $pagination = $paginator->paginate($query, $page, 20);
+
+            $response = $this->render('report/schedule.html.twig', [
+                'results' => $pagination,
+                'regions' => $regions,
+                'selectedRegions' => $dataFromRequest['regionIds'],
+                'dateFrom' => $dataFromRequest['dateFrom'],
+                'dateTo' => $dataFromRequest['dateTo'],
+            ]);
+        }
+
+        return $response;
+    }
+
+    /**
+     * @Route("/report/schedule/export", name="report.schedule.export",)
+     * @param $dateFrom
+     * @param $dateTo
+     * @param ScheduleReportManager $scheduleReportManager
+     * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
+     * @throws \PhpOffice\PhpSpreadsheet\Exception
+     * @throws \PhpOffice\PhpSpreadsheet\Writer\Exception
+     */
+    public function scheduleReportToExcel($dateFrom, $dateTo, $regionIds, ScheduleReportManager $scheduleReportManager)
+    {
+        $result = $scheduleReportManager->reportToExcel($dateFrom, $dateTo, $regionIds);
+
+        // Return the excel file as an attachment
+        return $this->file(
+            $result['file'],
+            $result['file_name'],
+            ResponseHeaderBag::DISPOSITION_INLINE
+        );
+    }
+}
