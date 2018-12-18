a627b26ca09ff0a6fff6f52811667853b5d7d2aa
Vaidas A <vaidastr@gmail.com>
2018-12-18 14:50:12 +0200

    Refactored fixtures. Added indexes to group address, timeslot date

--- src/Entity/LearningGroup.php
+++ src/Entity/LearningGroup.php
@@ -9,6 +9,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\LearningGroupRepository")
+ * @ORM\Table(indexes={@ORM\Index(name="group_address_idx", columns={"address"})})
  */
 class LearningGroup
 {
